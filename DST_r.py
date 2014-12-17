abafb9646409b56125b1e56c6137cc1a533bf558
A. R. Lass <andrew.r.lass@seagate.com>
2014-12-17 14:30:23 -0700

    single node hit working

--- DST_r.py
+++ DST_r.py
@@ -1,9 +1,5 @@
 
-node = node(int('001011',2),2,2,8)
-node.add_value(int('00101110',2)
-
-
-class node():
+class node:
 	def __init__(self, range_bits,node_length,headsh,key_length):
 		self.range_bits = range_bits << node_length # pad range_bits
 		self.node_length = node_length
@@ -11,21 +7,38 @@ class node():
 		self.key_length = key_length
 		self.num_shifts = (self.key_length-self.node_length)/self.head_shift
 		self.values = {}
+		#table containing masked bit ranges associated with other nodes
+		self.f_table = {}
 		
 	def get_value(self, key):
 		rbm = 0 #to store range bitmasked
 		kbm = 0#to store key bitmasked
-		bitmask = (2^headshift - 1) #fill with correct number of ones
-		bitmask = bitmask << (val_length) #skip over node stored bits
+		bitmask = (2^self.head_shift - 1) #fill with correct number of ones
+		bitmask = bitmask << (self.node_length + (self.num_shifts-1)*self.head_shift) #skip over node stored bits
 		#todo skip over alread processed bits
-		for shift in range(num_shifts,0,-1):
-			bitmask= bitmask << (self.head_shift*shift)
+		for i in range(0,self.num_shifts):
 			rbm = self.range_bits&bitmask
 			kbm = key&bitmask
+			print("offset %i" %i)
+			print("bitmask: %s" %bin(bitmask))
+			print("rbm: %s" %bin(rbm))
+			print("kbm: %s" %bin(kbm))
 			if rbm == kbm:
-				continue
+				bitmask = bitmask >> (self.head_shift)
 			else:
-				print("value not found contacting other node")
+				print("value not found contact other node")
+				return False
+		#if it gets here key is in node's range check table
+		if key in self.values:
+		  return self.values[key]
+		else:
+		  return False
 
 	def add_value(self,key,value):
+	  #todo check if it is in range
 		self.values[key]=value
+		
+		
+node = node(int('001011',2),2,2,8)
+node.add_value(int('00101110',2),True)
+print node.get_value(int('00101110',2))
